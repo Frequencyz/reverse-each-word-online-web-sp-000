8dc5d7cff89cf5e9273a794af022b9698110cd7c
Andrew Bay <daddnbay@yahoo.com>
2019-04-25 20:47:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ sentence1 = "Hello there, and how are you?"
 
 def reverse_each_word(sentence1)
   sentence1.each do |sentence|
-    return sentence1.reverse.split
+    return sentence1.reverse!
   end
 end
 
